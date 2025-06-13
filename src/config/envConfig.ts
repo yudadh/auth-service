7e11ac565f5772bec9b274429282404ac4e7d2a4
YudaDh <yudadarmawan26@gmail.com>
2025-06-13 19:58:00 +0800

    removing default ports

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -9,7 +9,7 @@ export const env = {
    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET || "your_refresh_secret",
    JWT_ACCESS_EXPIRY: process.env.JWT_ACCESS_EXPIRY || "15m",
    JWT_REFRESH_EXPIRY: process.env.JWT_REFRESH_EXPIRY || "7d",
-   PORT: process.env.PORT || 3000,
+   PORT: process.env.PORT,
    NODE_ENV: process.env.NODE_ENV || "production",
    GMAIL_USERNAME: process.env.GMAIL_USERNAME || '',
    GMAIL_PASSWORD: process.env.GMAIL_PASSWORD || ''
